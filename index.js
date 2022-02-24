590f9ecf62cacde22b0e84559a4e4488b00972f8
Shukhrat <96447783+MSLuckyBoy@users.noreply.github.com>
2022-02-24 19:22:17 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+function toggle() {
+	var sec = document.querySelector('#sec');
+	var nav = document.querySelector('#navigation');
+	sec.classList.toggle('active')
+	nav.classList.toggle('active')
+}
